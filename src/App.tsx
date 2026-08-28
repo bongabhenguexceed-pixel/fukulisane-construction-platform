b52543532e838d776eeef1526232fd670ea8e463
bongabhenguexceed-pixel <bongabhenguexceed@gmail.com>
2026-08-28 13:39:24 +0200

    test: verify push capability

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,1 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright (C) 2026 Shogo Technologies, Inc.
-import { useState, lazy, Suspense } from 'react'
-import StorefrontView from './components/StorefrontView'
-import IntegrationWizards from './components/IntegrationWizards'
+placeholder
