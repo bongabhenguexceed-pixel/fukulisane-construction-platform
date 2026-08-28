911672adedd16ab2bcb04f9640e0bff91ac2ca6b
bongabhenguexceed-pixel <bongabhenguexceed@gmail.com>
2026-08-28 08:44:41 +0200

    feat: Add entry point, lib utilities, and all UI components

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+// Copyright (C) 2026 Shogo Technologies, Inc.
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import { ShogoErrorBoundary } from './ShogoErrorBoundary'
+import './index.css'
+
+const root = document.getElementById('root')
+if (root) {
+  createRoot(root).render(
+    <ShogoErrorBoundary>
+      <App />
+    </ShogoErrorBoundary>,
+  )
+}
