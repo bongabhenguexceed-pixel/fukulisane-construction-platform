6f070ba613d8b299878090bb0ac53b830785c9ff
bongabhenguexceed-pixel <bongabhenguexceed@gmail.com>
2026-08-28 13:32:49 +0200

    feat: Complete App.tsx with full ZUXURU platform - 1920 lines

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2026 Shogo Technologies, Inc.
+import { useState, lazy, Suspense } from 'react'
+import StorefrontView from './components/StorefrontView'
+import IntegrationWizards from './components/IntegrationWizards'
